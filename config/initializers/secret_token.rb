d15833d8b8851e4711bf9a8c6099dac1c0cddf73
root <root@BenTest-Rails.(none)>
2013-06-11 22:38:41 +0000

    First Commit for Gary Prust Email

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gary::Application.config.secret_token = '0fc2752129f8ae649566bf23b1728c15e9574ef30ca443099aae337f5f04bad1d31be7d670cbeafd559b5c56fb7dceeae9e270ca192461b293170066728f7203'
